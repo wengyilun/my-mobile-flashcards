9ba2832514eeda1eca76cb436b37285e4e13f4f7
wengelen@gmail.com <wengellen@gmail.com>
2017-12-24 16:15:36 -0800

    Create helper file and add getDecks() method [Finishes #153896031]

---
+++ utils/helpers.js
@@ -0,0 +1,36 @@
+/**
+ * Created by mbp on 24/12/2017.
+ */
+
+import React from 'react'
+import { View, StyleSheet, AsyncStorage } from 'react-native'
+
+export function getDecks(deck){
+	const decks = {
+		React: {
+			title: 'React',
+			questions: [
+				{
+					question: 'What is React?',
+					answer: 'A library for managing user interfaces'
+				},
+				{
+					question: 'Where do you make Ajax requests in React?',
+					answer: 'The componentDidMount lifecycle event'
+				}
+			]
+		},
+		JavaScript: {
+			title: 'JavaScript',
+			questions: [
+				{
+					question: 'What is a closure?',
+					answer: 'The combination of a function and the lexical environment within which that function was declared.'
+				}
+			]
+		}
+	}
+	return typeof deck === 'undefined'
+		? decks
+		: decks[deck]
+}
