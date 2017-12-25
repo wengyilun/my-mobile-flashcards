5c80668073b697cacda97368ac255be78be6cd91
wengelen@gmail.com <wengellen@gmail.com>
2017-12-24 16:23:39 -0800

    Create reducers

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { getDecks } from './utils/helpers'
+import { Provider } from 'redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
   render() {
---
+++ reducers/index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by mbp on 24/12/2017.
+ */
+export function decks (state={}, action){
+	switch(action.type){
+		default:
+			return state
+	}
+}
+
+export function cards (state={}, action){
+	switch(action.type){
+		default:
+			return state
+	}
+}
+
+export function currentDeckIndex (state=0, action ){
+	switch(action.type){
+		default:
+			return state
+	}
+}
